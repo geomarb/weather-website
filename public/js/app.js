aeed6a2e08e28782d77d9a1ab03807b946911c95
geomarb <gtcbrs@gmail.com>
2019-05-03 17:43:46 +0100

    Remove empty lines

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 // const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit',(e) => {
     e.preventDefault()
     
