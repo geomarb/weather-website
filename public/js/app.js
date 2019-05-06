ad0fc9f9d5e8f5932d8f4e75a87277211f3e74c3
geomarb <gtcbrs@gmail.com>
2019-05-06 11:53:47 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ weatherForm.addEventListener('submit',(e) => {
     
     const location = search.value
 
-    fetch('/weather?address='+location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) return messageOne.textContent = data.error
             messageOne.textContent = data.forecast
